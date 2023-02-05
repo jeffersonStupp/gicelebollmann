810f5c41d4ca8a33e7b7ebdd9099468935d0b306
jeffersonStupp <107777402+jeffersonStupp@users.noreply.github.com>
2023-02-05 11:23:04 -0300

    05/02

--- src/app/COMPONENT/usuariocadastro/usuariocadastro.component.ts
+++ src/app/COMPONENT/usuariocadastro/usuariocadastro.component.ts
@@ -6,6 +6,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./usuariocadastro.component.css']
 })
 export class UsuariocadastroComponent implements OnInit {
+  nome:string="";
+  email:string="";
+  telefone:string="";
+  nickname:string="";
+  password:string="";
+  confpass:string="";
+
+  Cadastrar(){
+
+if(this.nome=""){
+  alert("Nome invalido")
+
+}else if(this.nome.length<3){
+  alert("nome invalido")
+}else{
+
+  console.log("nome:" , this.nome)
+}
+if(this.email=""){
+  alert("E-Mail invalido")
+
+}else if(this.email.indexOf("@")){
+  alert("E-Mail invalido")
+}else{
+  console.log("email:" , this.email)
+
+}
+
+
+  // console.log("telefone:" , this.telefone)
+  // console.log("nickname:" , this.nickname)
+  // console.log("password:" , this.password)
+
+  }
 
   constructor() { }
 
