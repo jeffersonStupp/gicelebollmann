c13bdc8e695de922213a6f7ac2548406694f7f1f
jeffersonStupp <107777402+jeffersonStupp@users.noreply.github.com>
2023-02-13 21:30:36 -0300

    validacoes

--- src/app/COMPONENT/usuariocadastro/usuariocadastro.component.ts
+++ src/app/COMPONENT/usuariocadastro/usuariocadastro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-usuariocadastro',
@@ -13,38 +14,51 @@ export class UsuariocadastroComponent implements OnInit {
   password:string="";
   confpass:string="";
 
-  Cadastrar(){
-
-if(this.nome.length<3){
+  AddUser(){
+    this.nome=this.nome
+if (this.nome==""){
+  alert("Nome não inserido")
+}
+else if(this.nome.length<3){
   alert("Nome invalido")
-
-
 }else{
-
   console.log("nome:" , this.nome)
 }
-if(this.email.length<3){
-  alert("E-Mail invalido")
-
-
+this.email=this.email
+if(this.email==""){
+alert("E-Mail não inserido")
+}
+else if(this.email.length<3){
+alert("E-Mail invalido")
 }else{
   console.log("email:" , this.email)
-
 }
+this.telefone=this.telefone
+if(this.telefone.length<=11){
+  alert("Numero de telefone inválido")
+}else{
+  console.log("telefone:" , this.telefone)
+}
+this.nickname=this.nickname
+console.log("nickname:" , this.nickname)
+this.password=this.password;
+this.confpass=this.confpass;
+
 if(this.password != this.confpass){
   alert("Senhas Invalidas")
 }else{
   console.log("password:" , this.password)
-}
+    }
 
 
-  // console.log("telefone:" , this.telefone)
-  // console.log("nickname:" , this.nickname)
-  //
+
+
 
   }
 
-  constructor() { }
+  constructor() {
+
+  }
 
   ngOnInit(): void {
   }
